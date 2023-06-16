9f8955a80bff2c16b276009fc3f3aaa46959cbb7
Pedro <pedrotmartin00@hotmail.com>
2023-06-16 16:57:29 -0300

    adicionado selecao de DDI

--- scripts/icc_selection.js
+++ scripts/icc_selection.js
@@ -1,7 +1,28 @@
 const items = document.querySelectorAll('li');
+const drop_input = document.querySelector('.dropdown_input');
 
 function icc_select(event){
-    console.log(event.target.textContent);
+    let drop_text = event.target.textContent.split('');
+
+    let k;
+    for(let i = 0; i<drop_text.length;  i++){
+        if (drop_text[i] == ' '){
+            k = i;
+            drop_text.splice(k);
+            break;
+        }
+    }
+
+    drop_text = drop_text.join('');
+
+    if(drop_text=='+XX'){
+        drop_input.value = '';
+        return;
+    }
+
+    drop_input.value = drop_text;
+    console.log(drop_text);
+
 }
 
 items.forEach(function(item){
